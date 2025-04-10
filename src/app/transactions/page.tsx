3c01786afcb124548ba9f98c1b027cfb625598c3
OnelioViera <you@example.com>
2025-04-10 16:19:19 -0600

    Add 'Annual Expenses' to transaction description options for expenses
    Enhance the transaction categorization by including 'Annual Expenses' in the expenses description options. This update improves the variety of choices available for users, ensuring a more comprehensive selection for transaction categorization.

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -67,6 +67,7 @@ const descriptionOptions: Record<"bill" | "expense" | "income", string[]> = {
     "Google One",
     "Dog Expenses",
     "Cursor",
+    "Annual Expenses",
   ],
   income: ["Salary", "Gifts", "Pension", "Daniel's Rent", "NuVet", "Other"],
 };
